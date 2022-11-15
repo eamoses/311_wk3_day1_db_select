19fd49753a72e0b7684698e774c348cdcbad37df
eamoses <e.a.moses@gmail.com>
2022-11-14 20:11:06 -0600

    save

--- first-instance.sql
+++ first-instance.sql
@@ -20,3 +20,45 @@ select *
 from users
 where phone1
 like '773%';
+
+-- order by
+select *
+from users
+order by last_name asc;
+
+-- select distinct
+select distinct(city)
+from users
+order by city;
+
+-- OR
+select *
+from users
+where city = 'Chicago'
+or city = 'Aberdeen';
+
+-- AND
+select *
+from users
+where city = 'Chicago'
+and last_name = 'Tollner';
+
+-- BETWEEN
+select *
+from users
+where id 
+between 200 
+and 500;
+
+-- IN
+select *
+from users
+where city
+in ('Chicago', 'Aberdeen');
+
+-- LIMIT
+select *
+from users
+where city = 'Chicago'
+order by last_name 
+limit 2;
